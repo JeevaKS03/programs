d3a23ef9c8e97c0bd5e266642e40576a5ec817df
JEEVA K S <145172065+JeevaKS03@users.noreply.github.com>
2025-10-21 09:10:28 +0530

    Refactor frequency counting logic using entrySet

--- Java/Intermediate/frequency_of_element_in_array.java
+++ Java/Intermediate/frequency_of_element_in_array.java
@@ -3,8 +3,8 @@ import java.util.HashMap;
 
 public class frequency_of_element_in_array {
     public static void main(String[] args) {
-    int[] arr = {2,2,4,6,8,6,3,3};
-    //Using Hashmap
+        int[] arr = {2,2,4,6,8,6,3,3};
+        //Using Hashmap
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int i=0;i<arr.length;i++){
             if(!map.containsKey(arr[i])){
@@ -15,10 +15,8 @@ public class frequency_of_element_in_array {
                 map.put(arr[i],temp+1);
             }
         }
-        Object[] array = map.values().toArray();
-        for(int i=0;i<map.size();i++){
-            System.out.println(arr[i]+" repeated :"+array[i]);
+        for (HashMap.Entry<Integer, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " repeated: " + entry.getValue());
         }
     }
-
 }
